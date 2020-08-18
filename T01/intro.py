8c1546b644e466924ccac0b33b5b4b7be5fd56e0
rafagr11 <rafael.garcia@mainscope.com>
2020-08-17 21:20:21 -0500

    Update intro.py
    Ya están el 2 el 4 y el 6 practicamente solo me faltaría saber como se toma la información de la matriz. Yo creo que podemos agregar al final un como wizard sencillo de que solo ponga que numero de ejercicio quiere probar desde el teclado y ya le da el resultado no?

--- T01/intro.py
+++ T01/intro.py
@@ -37,6 +37,19 @@ def sumMatrices(ma, mb):
 
 # sumMatrices(matrixA, matrixB)
 
+#----------ejercicio 2---------------
+
+
+def tuplaPares(tuplaT): #función que  recibe una tupla
+    temp=[] #se crea una lista vacia para agregar los elementos pares
+    a=() # tupla final
+    for indice, item in enumerate(tuplaT,1): #función que enumera lista
+        if(indice%2): 
+            #se resta 1 al indice ga que iniciamos del primer elemento
+            temp.insert(indice,tuplaT[indice-1]) 
+            a=tuple(temp) # se convierte la lista a una tupla
+    return a #regresa tupla de los elementos pares
+
 ######### 3. #########
 
 listA = ['uno', 2, 'tres', 8]
@@ -61,6 +74,22 @@ def intersection(listA, listB):
   print(hasValueInCommon)
 
 # intersection(listA, listB)
+#---------ejercicio 4------------------
+
+
+def cuentaPalabras(cadena): #función que recibe una cadena
+    extra=[] # se crea una lista auxiliar donde se ponen los datos finales
+    listaPalabras=cadena.split() #se crea una lista con los elementos de cade
+    x=0
+    frecuencia=[] # lista auxiliar para contar las frecuencias de cada palabra
+    for cuent in listaPalabras:
+        frecuencia.append(listaPalabras.count(cuent)) #lista de frecuencias d
+        #de cada palabra
+        extra.insert(x,(listaPalabras[x],frecuencia[x]))# se agregan a la list
+        #final los elementos de frecuencia y palabras
+        x=x+1
+    return dict(set(extra))#se convierte en diccionario los elementos de la li
+#utilizando la función set para quitar elementos repetidos
 
 ######### 5. #########
 def getObservationsCount(observedValuesList):
@@ -94,3 +123,56 @@ def rollDice(timesToRoll):
   print(countDictionary)
 
 # rollDice(8)
+
+#------------ejercicio 6---------------
+
+
+def multMatrices(a,b): #se reciben 2 listas con listas de cada vector(xyz)
+    r1=[]  # se crean listas auxiliares para almacenar los resultados
+    r2=[]
+    
+    while len(a)>0: #se recorre el # de veces del tamaño de a
+        recorrido=0 #variable para recorrer cada vector dentro de la matriz
+        a1=a[:1:] #se obtiene el primer vector
+        
+        
+        while recorrido<len(a1): #se recorre cada vector
+            for x in b: #recorrer cada vector dentro del # de columnas de b
+                for x1 in x: #recorre el numero de elementos anterior
+                    r1.append(x1*a1[0][recorrido])#se agrega al resultado la m
+                    #multiplicacion del elmento de laprimer fila de a por la 
+                    #columna de b
+                recorrido=recorrido+1# se recorre cada elemento
+        a.pop(0)# se elimina elemento de la lista
+        
+    r1=[r1[i:i+len(b[0])] for i in range(0,len(r1),len(b[0]))]
+    # range crea una lista desde 0 hasta el tamaño de la lista r1, avanzando 
+    #depende el tamño de be esa lista se recorre por i , y el valor actual 
+    # de i que avanza en la lista de resulados sumando con el el valor de la
+    #multiplicación almacenado en r1
+    
+    suma=0
+    
+    while len(r1)>0:#recorremos mientras r1>0 
+        for x in range(len(r1[0])):# se recorre una lista ordenada del numero
+       # de valores que tenemos en la lista de resultados r1
+            for y in range(len(b)):# se recorre cada vector dentr # col de b
+                suma=suma+r1[y][x] # se toman los valores de las multiplicaciones
+                #almacenados en r1 para sumar cada elemento por cada x y z
+            r2.append(suma)# se agregan los valor a lista de resultados r2
+            suma=0
+        for s in range(len(b)):
+            r1.pop(0)
+    
+    r2=[r2[i:i+len(b[0])] for i in range(0, len(r2),len(b[0]))]
+     # range crea una lista desde 0 hasta el tamaño de la lista r2, avanzando 
+    #depende el tamño de b esa lista se recorre por i , y el valor actual 
+    # de i que avanza en la lista de resulados  r2 sumando con el el valor de la
+    #multiplicación almacenado en r2 que se agregaron a traves de la variable 
+    #suma en cada iteracion 
+    
+    
+    return (r2)
+    
+        
+    
